1f5a922b8405c379ea5095a5c650b476bf80d946
PeacePeach <niknaim24@gmail.com>
2018-02-11 00:08:56 +0800

    Chapter 6 - Step 5: Improving Model Performance

---
+++ Machine Learning with R/Chapter 6 - wine.R
@@ -0,0 +1,172 @@
+# ----Classification and Regression Trees---
+# Background:
+# (a) Despite the name, regression trees do not use linear regression methods. 
+# (b) They make predictions based on the average value of examples that reach a leaf.
+# (c) Regression decision trees may be better suitted for tasks with many features and/or non-linear relationships.
+# (d) Partitioning is done using the divide-and-conquer strategy according to the feature that will result in the
+#     greatest increase in homogeneity in the outcome. 
+# (e) For numeric decision trees, homogeneity is measured by statistics such as var, sd, or abs dev from the mean.
+#     Common splitting criterion is called the Standard Deviation reduction (SDR).
+
+library(tidyverse)
+# ......SDR Example.....
+# Original 
+tee<- c(1, 1, 1, 2, 2, 3, 4, 5, 5, 6, 6, 7, 7, 7, 7)
+
+# Split A
+at1 <- c(1, 1, 1, 2, 2, 3, 4, 5, 5)
+at2 <- c(6, 6, 7, 7, 7, 7)
+
+# Split B
+bt1 <- c(1, 1, 1, 2, 2, 3, 4)
+bt2 <- c(5, 5, 6, 6, 7, 7, 7, 7)
+
+# Compute the SDR for each split
+sdr_a <- sd(tee) - (length(at1)/length(tee) * sd(at1) + length(at2)/length(tee)*sd(at2))
+sdr_b <- sd(tee) - (length(bt1)/length(tee) * sd(bt1) + length(bt2)/length(tee) * sd(bt2))
+
+# Compare
+sdr_a
+sdr_b
+#...
+
+# (f) Since the SDR is smaller (more homogeneous) in split B, the decision tree would use B first.
+# (g) Suppose that the tree stopped growing here, the regression tree's work is done. 
+# (h) One of the advantages of trees is that they can handle many types of data without preprocessing 
+#     (no need to normalize or standardize)
+
+# ----Example: Estimating the quality of wines with regression trees and model trees----
+
+# -----------------------------------------------------------------
+# Exploratory Data Analysis
+# -----------------------------------------------------------------
+white_wine <- read_csv("Machine Learning with R/Data/whitewines.csv")
+white_wine %>% glimpse()
+
+# Check the distribution of the quality
+white_wine %>% 
+  ggplot(aes(quality)) +
+  geom_bar(fill = "light blue", color = "black") +
+  ggtitle("Distribution of White Wine Quality") +
+  theme(axis.title = element_text(face = "bold", size = 13),
+        plot.title = element_text(face = "bold", size = 15, hjust = 0.5),
+        panel.background = element_rect(fill = "white"),
+        panel.grid.minor = element_blank(),
+        panel.grid.major.x = element_blank(),
+        panel.grid.major.y = element_line(color = "gray"),
+        axis.line.x = element_blank()) +
+  labs(x = "Quality", y = "Count")
+
+
+# -----------------------------------------------------------------
+# Training a model on the data
+# -----------------------------------------------------------------
+
+# Manual partitioning
+white_wine_train <- white_wine %>% slice(1:3750)
+white_wine_test <- white_wine %>% slice(3751:n())
+
+# Strata sampling
+library(caTools)
+set.seed(123)
+split <- sample.split(white_wine$quality, SplitRatio = 0.7656186)
+wine_train <- white_wine %>% filter(split)
+wine_test <- white_wine %>% filter(!split)
+
+# Comparing the strata distribution with the complete dataset
+white_wine %>% select(quality) %>% table() %>% prop.table() * 100
+wine_train %>% select(quality) %>% table() %>% prop.table() * 100
+white_wine_train %>% select(quality) %>% table() %>% prop.table() * 100
+wine_test %>% select(quality) %>% table() %>% prop.table() * 100
+white_wine_test %>% select(quality) %>% table() %>% prop.table() * 100
+
+
+# Build our cart model
+library(rpart)
+m.rpart <- rpart(quality ~ ., data = white_wine_train)
+
+my_model <- rpart(quality ~ ., data = wine_train)
+
+# .....Interpreting the model.....
+# The number of examples reaching each node is indicated after the splitting condition.
+# Node --> split, n, deviance, yval.
+# Deviance = maximizing the between group SSE (sum of sequares).
+# The '*' indicates a terminal of leaf node, which means they result in a prediction.
+m.rpart
+my_model
+
+# Visualizing decision trees
+library(rpart.plot)
+rpart.plot(m.rpart, digits = 3)
+
+# fallen.leaves forces the leaf nodes to be alligned at the bottom of the plot
+rpart.plot(m.rpart, digits = 4, fallen.leaves = TRUE, type = 3, extra = 101)
+rpart.plot(my_model, digits = 4, fallen.leaves = TRUE, type = 3, extra = 101)
+
+
+# -----------------------------------------------------------------
+# Evaluating Model Performace
+# -----------------------------------------------------------------
+
+p.rpart <- predict(m.rpart, white_wine_test)
+my_prediction <- predict(my_model, wine_test)
+
+# The findings suggests that the model is not correctly identifying the extreme cases 
+summary(p.rpart)
+summary(white_wine$quality)
+summary(my_prediction)
+
+# Use correlation to measure the relationship between the predictions and the true value
+cor(p.rpart, white_wine_test$quality)
+cor(my_prediction, wine_test$quality)
+
+
+# .....Measuring performance with the mean absolute error.....
+# (a) Another way to think about the model's performance is to consider how far (avg) its prediction from true value.
+# (b) This measurement is called the mean absolute error (MAE)
+
+#Create MAE function
+MAE <- function(actual, predicted) {
+  mean(abs(actual - predicted))
+}
+
+MAE(p.rpart, white_wine_test$quality)
+MAE(my_prediction, wine_test$quality)
+
+# (c) The result implies that on average the difference between our model's performance and actual score is 0.587.
+# (d) If we predict the mean value of our training sample, we wil be off by 0.67.
+# (e) Cortez reported MAE of 0.58 for Neural Network and 0.45 for SVM
+MAE(mean(white_wine_train$quality), white_wine_test$quality)
+
+
+
+# -----------------------------------------------------------------
+# Improving Model Performance
+# -----------------------------------------------------------------
+# A model tree improves on regression trees by replacing the leaf nodes with regression models
+# The current state-of-the-art in model trees is the M5' algorithm (M5-prime)
+library(RWeka)
+m.m5p <- M5P(quality ~ ., data = white_wine_train)
+m.m5p
+
+# The split can be very similar to the regression tree.
+# A key difference, however is that the nodes terminate not in a numeric prediction, but a linear model. 
+# The values can be interpreted exactly the same as multiple regression models. 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
