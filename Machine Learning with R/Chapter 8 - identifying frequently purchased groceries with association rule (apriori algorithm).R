e877f36fe6f6ded2e36a48c6c2d57c04b595a13d
PeacePeach <niknaim24@gmail.com>
2018-03-08 23:55:59 +0800

    Market Basket Analysis using Association Rules - Part 3.

--- Machine Learning with R/Chapter 8 - identifying frequently purchased groceries with association rule (apriori algorithm).R
+++ Machine Learning with R/Chapter 8 - identifying frequently purchased groceries with association rule (apriori algorithm).R
@@ -8,8 +8,37 @@ library(arules)
 # Exploratory Data Analysis
 # -----------------------------------------------------------------
 # We will need to use sparse matrix for this type of datasets and problem. 
-# We can use  the function from arules package to accomplish this. 
+# We can use  the function 'read.transactions' from arules package to accomplish this. 
 groceries <- read.transactions("Machine Learning with R/Data/groceries.csv", sep = ",")
 groceries %>% summary()
 
 # In the summary ouput, rows refer to number of transactions. Desity refers to proportion of nonzero matrix. 
+# We use 'inspect()' function to look at the contents of the sparse matrix
+inspect(groceries[45:50])
+
+# Use the 'itemFrequency()' function to see the proportion of transaction that contains the item.
+itemFrequency(groceries[,1:3])
+
+# To visualize the data use 'itemFrequencyPlot()'. Use the parameter 'support' or 'topN' for filtering products
+itemFrequencyPlot(groceries, support = 0.1)
+itemFrequencyPlot(groceries, topN = 20)
+
+# It's also possible to visualize the whole sparse matrix. 
+image(groceries[1:5])
+image(groceries[1:100])
+image(sample(groceries, 100))
+
+
+# -----------------------------------------------------------------
+# Training a model on the data
+# -----------------------------------------------------------------
+groceryrules <- apriori(groceries, parameter = list(support = 0.006, confidence = 0.25, minlen = 2))
+groceryrules
+
+
+
+
+
+
+
+
